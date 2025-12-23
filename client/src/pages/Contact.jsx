dad8c660ad727da0c70f1feeafef74684c343f5e
myscreencode18 <147525475+myscreencode18@users.noreply.github.com>
2025-12-23 10:22:37 +0530

    frontend url fix

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -44,7 +44,7 @@ const Contact = () => {
   try {
     setLoading(true);
 
-    const res = await fetch("http://localhost:5000/api/contact", {
+    const res = await fetch( `${import.meta.env.VITE_API_URL}/api/contact`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
